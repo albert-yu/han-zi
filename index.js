f1a7f79326d3965fb7895e548fb7d5af3306e544
Albert Yu <albert.yu@gmail.com>
2026-05-08 16:27:54 +0800

    ignore header

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 async function fetchCSV() {
 	const resp = await fetch("./phrases/intro.csv");
 	const text = await resp.text();
-	const rows = text.split("\n").filter((line) => !!line);
+	// ignore header and empty lines
+	const rows = text.split("\n").filter((line, i) => i > 0 && !!line);
 	const csvArray = rows.map((row) => row.split(","));
 	console.log("csvArray", csvArray);
 }
